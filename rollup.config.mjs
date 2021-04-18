9dfc3d466654b8f1bd95d21f338fc7ad25594e49
Léonard <leonard@bigsool.com>
2021-04-18 03:22:01 +0200

    partial dev server fix

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,26 +3,37 @@ import {terser} from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
+import process from 'process';
 
 // import {localeTransformers} from '@lit/localize-tools/lib/rollup.js';
 
 import copy from 'rollup-plugin-copy';
 
+const targets = [
+  { src: 'node_modules/@webcomponents', dest: 'dist/node_modules/' },
+  { src: 'node_modules/@lit/reactive-element/polyfill-support.js', dest: 'dist/node_modules/@lit/reactive-element/' },
+  { src: 'dev/assets', dest: 'dist' },
+  { src: 'dev/index.html', dest: 'dist' },
+  { src: 'dev/*.png', dest: 'dist/assets' },
+  { src: 'dev/*.ico', dest: 'dist/assets' },
+  { src: 'dev/*.svg', dest: 'dist/assets' },
+  { src: 'dev/*.jpeg', dest: 'dist/assets' },
+  { src: 'dev/site.webmanifest', dest: 'dist/assets' },
+  { src: 'dev/index.css', dest: 'dist/assets' },
+  { src: 'dev/tailwind.css', dest: 'dist/assets' },
+  { src: 'dev/robots.txt', dest: 'dist' },
+];
+
+const isDev = process.env.BUILD === 'development';
+
+if(isDev){
+  targets.push(
+    { src: 'luna-orbit.js', dest: 'dev/assets' },
+  )
+}
+
 const copyConfig = {
-  targets: [
-    { src: 'node_modules/@webcomponents', dest: 'dist/node_modules/' },
-    { src: 'node_modules/@lit/reactive-element/polyfill-support.js', dest: 'dist/node_modules/@lit/reactive-element/' },
-    { src: 'dev/assets', dest: 'dist' },
-    { src: 'dev/index.html', dest: 'dist' },
-    { src: 'dev/*.png', dest: 'dist/assets' },
-    { src: 'dev/*.ico', dest: 'dist/assets' },
-    { src: 'dev/*.svg', dest: 'dist/assets' },
-    { src: 'dev/*.jpeg', dest: 'dist/assets' },
-    { src: 'dev/site.webmanifest', dest: 'dist/assets' },
-    { src: 'dev/index.css', dest: 'dist/assets' },
-    { src: 'dev/tailwind.css', dest: 'dist/assets' },
-    { src: 'dev/robots.txt', dest: 'dist' }
-  ],
+  targets,
   verbose: true,
 };
 
