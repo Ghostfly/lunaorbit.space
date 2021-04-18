7e90b674cd8e5f4a982694a42d7cd7d47899a5b4
Léonard <leonard@bigsool.com>
2021-04-18 17:36:43 +0200

    Fix localize

--- src/x-equation.ts
+++ src/x-equation.ts
@@ -7,6 +7,7 @@ import {
 import {
   Localized
 } from '@lit/localize/localized-element';
+import { msg } from '@lit/localize';
 
 @customElement('x-equation')
 export class XEquation extends Localized(LitElement) {
@@ -18,14 +19,14 @@ export class XEquation extends Localized(LitElement) {
     return html`
       <div class="container  mx-auto py-12 px-6 flex justify-between">
         <ul class="container px-2 py-2 mx-auto">
-          <li><em class="font-semibold"><a class="text-gray-600" href="https://terra.money" target="_blank" rel="noopener">Terra</a></em>'s competition is the <em class="font-semibold">Dollar</em>.</li>
-          <li><em class="font-semibold"><a class="text-gray-600" href="https://mirror.finance" target="_blank" rel="noopener">Mirror</a></em>'s competition is the <em class="font-semibold">NASDAQ</em>.</li>
-          <li><em class="font-semibold"><a class="text-gray-600" href="https://anchorprotocol.com" target="_blank" rel="noopener">Anchor</a></em>'s competition is the <em class="font-semibold">Federal Funds Rate</em>.</li>
+          <li><em class="font-semibold"><a class="text-gray-600" href="https://terra.money" target="_blank" rel="noopener">${msg(`Terra`)}</a></em>${msg(`'s competition is the`)}<em class="font-semibold">${msg(`Dollar`)}</em>.</li>
+          <li><em class="font-semibold"><a class="text-gray-600" href="https://mirror.finance" target="_blank" rel="noopener">${msg(`Mirror`)}</a></em>${msg(`'s competition is the`)} <em class="font-semibold">${msg(`NASDAQ`)}</em>.</li>
+          <li><em class="font-semibold"><a class="text-gray-600" href="https://anchorprotocol.com" target="_blank" rel="noopener">${msg(`Anchor`)}</a></em>${msg(`'s competition is the`)} <em class="font-semibold">${msg(`Federal Funds Rate`)}</em>.</li>
         </ul>
         <div class="flex justify-center m-4 items-center">
           <a class="terra-color" href="https://docs.terra.money/luna.html">
             <img class="h-5" src="/assets/luna.svg" alt="Luna logo" width="20" height="20" />
-            <h3 class="font-semibold ml-2">Luna governs them all !</h3>
+            <h3 class="font-semibold ml-2">${msg('Luna governs them all !')}</h3>
           </a>
         </div>
     </div> 
