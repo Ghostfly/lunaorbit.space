911732b7488bc17a7ab078eb14074ebbb8826ac8
Léonard <leonard@bigsool.com>
2021-04-18 16:35:53 +0200

    Add french translation

--- src/locales/fr.ts
+++ src/locales/fr.ts
@@ -8,129 +8,79 @@
     /* eslint-disable @typescript-eslint/no-explicit-any */
 
     export const templates = {
-      's0a8ca47bbbdc78bb': `Stake with us today !`,
-'s93d85c9969bc1e8e': `Get started`,
-'s342e3f24c1234918': `Safe`,
-'sd88dd390f024204f': `Multi-cloud hosting across 12 regions on AWS, GCP, and Azure with 24/7 monitoring`,
-'s6f65e19c3b6c30ae': `Distributed`,
-'sea5fe44851d08155': `Geographically distributed across four continents to ensure validator stays online.`,
-'s3ceed4d952789f32': `Smart`,
-'s2012a58d9ed2b0dc': `Oracle Management provides maximum accuracy to maximize delegation rewards`,
-'s198f498517ea3a13': `Fair`,
-'s57c6c9fa579d63f8': `0% commissions until`,
-'s7e2477ab41df485a': `May 10th 2021`,
-'sa24a6e713286b437': `, then 2%.`,
-'s1ec1b6a8da184889': `Aware`,
-'s157787c7bf8aa808': `Changes in the commission rate will be announced 30 days in advance.`,
-'sa55a23781c05de90': `Reachable`,
-'s1915bd2be4e757ad': `Find us on the Luna Orbit chat on`,
-'s6632bee996b65d13': `How to start staking on Terra ?`,
-'sa07f6db2301a6ccb': `Download Terra Station`,
-'s28aeca54f10f2b0d': `Choose the validator`,
-'sc22e8285e4ff2ba8': `Delegate tokens`,
-'sb9a367840a904f1b': `Voting power`,
-'s94e9e7ab102beee3': `This is the amount of Luna that has been validated to this
-                  node. For the sake of decentralization, it is better if this
-                  number is lower. If any one node has too much voting power,
-                  they have too much influence over voting.`,
-'sa41e9d2cdd95fed8': `Self-delegation`,
-'s446a0dc2e2a550eb': `
-                    This is skin in the game, but the number reported here is
-                    often not correct. For Luna Orbit, I am currently 10% self
-                    delegated (as of March 2021) but will be 25% within a month.
-                    If you find skin in the game important, I recommend reaching
-                    out to the validator to ask them the accurate number they have
-                    delegated.
-                  `,
-'sb3c11ece1101551e': `Validator commission`,
-'sf05fce28d5ea7456': `
-                  This is currently the only way a validator makes money and it
-                  comes out of rewards. For example, at a rate of commission at
-                  5% and a reward of 1 Luna, a delegator would receive .95 Luna
-                  and the validator would receive .05 Luna. Luna Orbit utilizes
-                  enterprise grade hosting that should ensure maximum
-                  reliability and performance but it comes at a cost. Delegators
-                  must decide if reliability and performance is important to
-                  them or if lower cost validators are worth the risk.
-                  `,
-'sb558ab601f7fc437': `Delegation return`,
-'sb81b5dc04545b551': `
-                  This is the current annual return you can expect from staking
-                  with this validator. Keep in mind that validators that are new
-                  (less than 30 days) might show different actual rates. This
-                  does not mean that a new delegator making 12% is better than
-                  an established validator making 8%, it just means the returns
-                  have gone up recently and the numbers fail to fairly represent
-                  the true picture for delegation return. However - Do Kwon has
-                  said that oracle performance will have a major impact on
-                  delegation returns so pay attention to the performance of your
-                  validators at this website (Look at http://terra.stake.id and
-                  check the column "Missed Oracle Votes" to get an idea of
-                  performance).
-                  `,
-'s1ccedf72cf7a2cd9': `Uptime`,
-'sf0f18f40d187045d': `
-                  This might be the most important on the list, you want the
-                  maximum amount of uptime. Anything less can lead to small
-                  penalties "slashing" to "jailing" . A small penalty is .01%
-                  while jailing would cut off rewards until you redelegate or
-                  the node fixes the issues. You want to choose a node that is
-                  reliable if you want good uptime.
-                  `,
-'s56e94b8c69f4b0f1': `The Blue check mark`,
-'s352ce7b5ba1016c8': `This means the validator has submitted a profile on github. This is cosmetic only and has no effect on anything.`,
-'s665a32ac519c0d9e': `Other considerations`,
-'s4457b0120ecd486d': `
-                  Some validators are active in the community and help people
-                  out, some are building useful tools, some are high end for
-                  reliability, and some people just want to make sure they
-                  support the smaller validators in order to decentralize. There
-                  could be any number of reasons to choose particular validators
-                  and they all have an incentive to help you and the Terra eco
-                  system!
-                  `,
-'s4c9c3de1df07cb94': `Discover & Understand Terra`,
-'sc26501e3ade7da3c': `Tools`,
-'s0f5cbda53ab0b573': `Airdrop timing and information`,
-'s46df4b2d546f3067': `I use this to follow upcoming airdrops you get for staking Luna in Terra Station.  As of April 9th 2021 there are 2 drops per week - ANC and MIR.`,
-'s420631e1ce34c8fa': `Vercel Airdrop Timer`,
+      's08b05407b5565ca4': `ou`,
+'s0a8ca47bbbdc78bb': `Stakez avec nous aujourd'hui !`,
 's0c0f4361a2eb76ed': `Terra Analytics`,
-'s5e9dc507e4fba96f': `Staking / Unstaking Calendar`,
-'s78af760bf3aa0d23': `Shows the amount of Luna that has been delegated or undelegated on certain days.  When Luna gets undelegated and 21 days pass, it is possible that the Luna will get moved to an exchange and sold. Track`,
-'scd40e9bdb7f30072': `Exchange Wallets`,
-'sa4d15873cde6a691': `As Luna gets burned, the amount of Luna available on exchanges should slowly decrease.  Also good to watch these addresses during big undelegation events.`,
-'s28106bfb3d9d03d7': `Various useful metrics for Anchor and TerraSwap`,
-'sb85d1541cfa12d5c': `You can see Anchor earn APY history, tokens in the ANC - UST LP,  bLuna / Luna swap price history and pool size, etc.`,
-'sa56522d944842d2c': `UST Supply information`,
-'se7974e3fee0ca5a0': `This is the most important metric to watch, UST market cap and growth day over day.  Burn tracking shows you how much Luna has been burned, more is better.`,
-'sa956e62593dd02d8': `Mirror volume`,
-'s3383ecf9c0006ebb': `As the price of Mirror goes up, so does the yield for staking mAssets which in turn drives demand for UST and increases the value of the weekly MIR airdrop.`,
-'sce94f12c12fa2d8f': `Validator performance`,
-'sc8c6e4d184303c3c': `How is Luna Orbit doing?  You want to see good uptime, few missed Oracle votes, and low precommits missed.  Numbers are trailing and should be watched over time.`,
-'se1fe3a61e8d02690': `Terra Stake ID`,
+'s0f5cbda53ab0b573': `Informations sur les Airdrops`,
+'s157787c7bf8aa808': `Les modifications du taux de commission seront annoncées 30 jours à l'avance.`,
+'s1915bd2be4e757ad': `Retrouvez-nous sur le chat Luna Orbit `,
+'s198f498517ea3a13': `Juste`,
+'s1ccedf72cf7a2cd9': `Disponibilité`,
+'s1ec1b6a8da184889': `Pragmatique`,
+'s2012a58d9ed2b0dc': `Utilisation d'oracles décentralisés pour offrir une précision maximale et maximiser les récompenses de délégation`,
+'s202cc9aafc815aae': `Outils Terra`,
+'s28106bfb3d9d03d7': `Métriques utiles pour Anchor & TerraSwap`,
+'s28aeca54f10f2b0d': `Choisissez un validateur`,
+'s3383ecf9c0006ebb': `À mesure que le prix de Mirror augmente, le rendement des actifs stakés augmente également, ce qui à son tour stimule la demande d'UST et augmente la valeur du airdrop hebdomadaire MIR`,
+'s342e3f24c1234918': `Sûr`,
+'s352ce7b5ba1016c8': `Cela signifie que le validateur a soumis un profil sur github. Ceci est purement cosmétique et n'a aucun effet sur quoi que ce soit.`,
+'s3ceed4d952789f32': `Intelligent`,
+'s420631e1ce34c8fa': `Vercel Airdrop Timer`,
+'s4457b0120ecd486d': `Certains validateurs sont actifs dans la communauté et aident au maximum, certains construisent des outils utiles, certains sont haut de gamme au niveau de la fiabilité, et certaines personnes veulent simplement s'assurer de soutenir les plus petits validateurs afin de décentraliser. Voilà quelques raisons pour vous aider à choisir des particuliers dans l'intérêt d'aider, vous et l'écosystème Terra!`,
+'s446a0dc2e2a550eb': `Le nombre indiqué est souvent incorrect. Pour Luna Orbit, je suis actuellement à 10% moi-même délégué (à partir de mars 2021) mais serait à 25% d'ici un mois. Si vous trouvez cette mesure importante, je vous recommande d'atteindre le validateur pour lui demander le nombre de tokens qu'ils ont délégué.`,
+'s46df4b2d546f3067': `Je l'utilise pour suivre les prochains airdrops que vous obtenez pour votre staking Luna dans Terra Station. Depuis le 9 avril 2021, il y a 2 drops par semaine - ANC et MIR.`,
+'s4c9c3de1df07cb94': `Découvrir & comprendre Terra`,
+'s56e94b8c69f4b0f1': `La coche bleue`,
+'s57adf424d57c8a0f': `Envoyer`,
+'s57c6c9fa579d63f8': `0% de frais jusqu'au`,
+'s5e9dc507e4fba96f': `Calendrier de delegate / undelegate`,
 's605e1718af12083a': `Luna Orbit - hubble.figment.io`,
-'sfa3a1693161dda38': `Blep.ai - LUNA-UST (UniSwap)`,
-'sa96f3a6a684e87c8': `Blep.ai - MIR-UST (UniSwap)`,
-'s202cc9aafc815aae': `Terra tools`,
-'s7dbad456eb3aabea': `To understand why Luna could appreciate over time, you need to
-                understand a few things. The first thing is to understand
-                the relationship between the market cap of UST and the price of
-                Luna. The second piece of the puzzle is to understand all
-                of the different ways demand is being created for the adoption
-                and use of UST. When you put those two pieces together,
-                you will understand why Luna could continue to appreciate in
-                value over time. These are the tools that I personally use
-                to watch the ecosystem around Terra. If you have any
-                questions, please come find me in the`,
-'sd0a48cb7c6a059b2': `Luna Orbit chat room on Telegram`,
-'s895035c1fab2b964': `Justin`,
-'sc1e59c37f65f8a0b': `We are here to help.`,
+'s6632bee996b65d13': `Comment staker sur Terra ?`,
+'s665a32ac519c0d9e': `Autres considérations`,
+'s6f65e19c3b6c30ae': `Distribué`,
+'s78af760bf3aa0d23': `Affiche le montant de Luna qui a été délégué ou non délégué. Lorsque Luna n'est pas déléguée et que 21 jours passent, il est possible que Luna soit déplacée vers un échange et vendue (Luna redevient liquide)`,
+'s7dbad456eb3aabea': `Pour comprendre pourquoi Luna pourrait s'apprécier au fil du temps, vous devez comprendre quelques choses. 
+La première chose est de comprendre la relation entre la capitalisation boursière de l'UST et le prix de Luna. 
+La deuxième pièce du puzzle est de comprendre les différentes façons dont la demande est créée pour l'adoption et l'utilisation de l'UST. 
+Lorsque vous assemblez ces deux pièces, vous comprenez pourquoi Luna peut continuer à s'apprécier en valeur au fil du temps. Ce sont les outils que j'utilise personnellement pour observer l'écosystème autour de Terra. Si vous avez des questions, venez me trouver dans le`,
+'s7e2477ab41df485a': `10 Mai 2021`,
 's87bae2710b2492c3': `Contact`,
-'sd15de0dcd2378a55': `Contact us`,
-'sd1f44f1a8bc20e67': `Email`,
-'saf84ec86139d349e': `Comment`,
-'s57adf424d57c8a0f': `Send`,
-'sd172ebbd02baf0fb': `Find us on`,
-'s08b05407b5565ca4': `or`,
+'s895035c1fab2b964': `Justin`,
+'s93d85c9969bc1e8e': `Commencer`,
+'s94e9e7ab102beee3': `C'est la quantité de Luna qui a été validée par ce nœud. 
+Dans un souci de décentralisation, il vaut mieux que le nombre soit inférieur. 
+Si l'un des nœuds a trop de pouvoir de vote, ils ont trop d'influence sur le vote.`,
+'sa07f6db2301a6ccb': `Télécharger Terra Station`,
+'sa24a6e713286b437': `, puis 2%`,
+'sa41e9d2cdd95fed8': `Autodélégation`,
+'sa4d15873cde6a691': `Comme Luna est brûlé, la quantité de Luna disponible sur les échanges doit baisser. Il faut regarder ces adresses pendant les gros évènements de délégation.`,
+'sa55a23781c05de90': `Accessible`,
+'sa56522d944842d2c': `Informations sur l'approvisionnement en UST`,
+'sa956e62593dd02d8': `Volume Mirror`,
+'sa96f3a6a684e87c8': `Blep.ai - MIR-UST (UniSwap)`,
+'saf84ec86139d349e': `Message`,
+'sb3c11ece1101551e': `Commission du validateur`,
+'sb558ab601f7fc437': `Retour de délégation`,
+'sb81b5dc04545b551': `Il s'agit du rendement annuel actuel que vous pouvez attendre du staking avec ce validateur. Gardez à l'esprit que les validateurs récents (moins de 30 jours) peuvent afficher des taux réels différents. Cette mesure ne veut pas dire qu'un nouveau délégant gagnant 12% vaut mieux que un validateur établi faisant 8%, cela signifie juste que les retours ont augmenté récemment et les chiffres ne représentent pas équitablement la vraie image pour le retour de la délégation. Cependant - Do Kwon a déclaré que les performances d'Oracle auront un impact majeur sur la délégation alors faites attention à la performance de votre validateur sur ce site Web (Regardez http://terra.stake.id et cochez la colonne "Votes Oracle manqués" pour avoir une idée de performance).`,
+'sb85d1541cfa12d5c': `Vous pouvez voir l'historique APY d'Anchor, les tokens dans les pools de liquidités l'ANC-UST, l'historique des prix d'échange bLuna / Luna, la taille du pool, etc.`,
+'sb9a367840a904f1b': `Pouvoir de vote`,
+'sc1e59c37f65f8a0b': `Nous sommes là pour aider.`,
+'sc22e8285e4ff2ba8': `Déléguer vos tokens`,
+'sc26501e3ade7da3c': `Outils`,
+'sc8c6e4d184303c3c': `Comment va Luna Orbit? Vous voulez voir une bonne disponibilité, peu de votes Oracle manqués et de faibles pré-engagements manqués. Les chiffres sont à la traîne et devraient être surveillés au fil du temps.`,
+'scd40e9bdb7f30072': `Portefeuilles d'exchanges`,
+'sce94f12c12fa2d8f': `Performance du validateur`,
+'sd0a48cb7c6a059b2': `Chat Luna Orbit sur Telegram`,
+'sd15de0dcd2378a55': `Contactez-nous`,
+'sd172ebbd02baf0fb': `Retrouvez-nous sur`,
+'sd1f44f1a8bc20e67': `E-mail`,
+'sd88dd390f024204f': `Hébergement cloud sur 12 regions avec AWS, GCP et Azure avec monitoring 24/7`,
+'se1fe3a61e8d02690': `Terra Stake ID`,
+'se7974e3fee0ca5a0': `Il s'agit de la mesure la plus importante à surveiller, la capitalisation boursière UST et la croissance au jour le jour. Le suivi des brûlures vous montre combien de Luna a été brûlé.`,
+'sea5fe44851d08155': `Réparti sur 4 continents pour assurer que le validateur reste en ligne`,
+'sf05fce28d5ea7456': `C'est actuellement le seul moyen pour un validateur de gagner de l'argent et d'obtenir des récompenses. 
+Par exemple, à un taux de commission de 5% et une récompense de 1 Luna, un délégant recevra 0,95 Luna et le validateur recevra 0,05 Luna. Luna Orbit utilise un hébergement de qualité entreprise qui devrait assurer un maximum fiabilité et performances, mais cela a un coût. Les délégants doivent décider si la fiabilité et la performance sont importantes pour eux ou si les validateurs à moindre coût valent le risque.`,
+'sf0f18f40d187045d': `C'est peut-être le plus important de la liste, vous voulez le quantité maximale de disponibilité. Moins peut conduire à des peines : «slashing» => «jailing». Une petite pénalité est de 0,01% tandis que l'emprisonnement couperait les récompenses jusqu'à ce que vous soyez re-délégué ou que le nœud arrive à résoudre les problèmes. Vous souhaitez choisir un nœud qui est fiable si vous voulez une bonne disponibilité.`,
+'sfa3a1693161dda38': `Blep.ai - LUNA-UST (UniSwap)`,
     };
   
