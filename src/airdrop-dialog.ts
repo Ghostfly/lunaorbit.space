22935f61594d2699b6fbcdbd43842f2e92e4454f
Léonard <leonard@bigsool.com>
2021-04-18 23:19:26 +0200

    fix missing redirect

--- src/airdrop-dialog.ts
+++ src/airdrop-dialog.ts
@@ -13,6 +13,7 @@ import {
 import {Localized} from '@lit/localize/localized-element';
 import {msg} from '@lit/localize';
 import {AnchorClaimResponse, MIRAirdrop, TerraQuery} from './terra-min';
+import { Router } from '@vaadin/router';
 
 /**
  * Airdrop dialog component
@@ -256,6 +257,7 @@ export class AirdropDialog extends Localized(LitElement) {
               }
               <button @click=${() => {
                 this.close();
+                Router.go('home');
               }} type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                 ${msg('Close')}
               </button>
