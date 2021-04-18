b0adcb097431bc010c9d82da4d78d7d66d8895af
Léonard <leonard@bigsool.com>
2021-04-18 20:04:24 +0200

    fix i18n

--- src/localization.ts
+++ src/localization.ts
@@ -3,7 +3,9 @@
 */
 
 import { configureLocalization } from '@lit/localize';
-import {sourceLocale, targetLocales} from './locale-codes';
+import { sourceLocale, targetLocales } from './locale-codes';
+
+import getUserLocale from 'get-user-locale';
 
 export const {getLocale, setLocale} = configureLocalization({
   sourceLocale,
@@ -12,7 +14,11 @@ export const {getLocale, setLocale} = configureLocalization({
 });
 
 export const setLocaleFromUrl = async (): Promise<void> => {
+  const navigatorLanguage = getUserLocale();
+
+  const deducedLanguage = navigatorLanguage.split('-')[0];
   const url = new URL(window.location.href);
-  const locale = url.searchParams.get('locale') || sourceLocale;
+  const locale = url.searchParams.get('locale') || sourceLocale || deducedLanguage;
+
   await setLocale(locale);
 };
