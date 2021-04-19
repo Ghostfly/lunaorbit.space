df4a1497e450b0f36a7f78b90a58f7f6d15e0055
Léonard <leonard@bigsool.com>
2021-04-19 19:23:05 +0200

    add storage class

--- src/storage.ts
+++ src/storage.ts
@@ -1,7 +1,57 @@
-import { Storage } from '@stacks/storage';
+import { GetFileOptions, GetFileUrlOptions, PutFileOptions, Storage } from '@stacks/storage';
 import { userSession } from './auth';
 
-const storage = new Storage({ userSession });
-// const blockStackUsername = '1CvACptXN5K9nJbTQHpWf5sA4MXGEQv3h9';
+export const storage = new Storage({ userSession });
+export const blockStackUsername = '1CvACptXN5K9nJbTQHpWf5sA4MXGEQv3h9';
 
-console.warn('Storage ready', storage);
+export async function putFile(name: string, data: string | Buffer | ArrayBufferView | Blob, options: PutFileOptions): Promise<string> {
+  const file = await storage.putFile(name, data, options);
+
+  return file;
+}
+
+export async function getFile(name: string, options: GetFileOptions): Promise<string | ArrayBuffer | null> {
+  const file = await storage.getFile(name, options);
+
+  return file;
+}
+
+export async function getFileURL(name: string, options: GetFileUrlOptions): Promise<string> {
+  const file = await storage.getFileUrl(name, options);
+
+  return file;
+}
+
+export async function getFileContents(path: string, app: string, username: string | undefined, zoneFileLookupURL: string | undefined, forceText: boolean): Promise<string | ArrayBuffer | null> {
+  const fileContents = await storage.getFileContents(path, app, username, zoneFileLookupURL, forceText);
+
+  return fileContents;
+}
+
+export async function deleteFile(name: string, options?: {
+  wasSigned: boolean | undefined;
+}): Promise<boolean> {
+  await storage.deleteFile(name, options);
+
+  return true;
+}
+
+export async function listFiles(name?: string): Promise<string | string[] | ArrayBuffer | null> {
+  const files: string[] = [];
+
+  storage.listFiles((filename) => {
+    if (name && name === filename) {
+      return true;
+    }
+
+    files.push(filename);
+
+    return false;
+  });
+
+  if (name) {
+    return await storage.getFile(name);
+  }
+
+  return files;
+}
