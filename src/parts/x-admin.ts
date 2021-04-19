9b0717ac0974781bd6f56d97de93b7b4d6243a47
Léonard <leonard@bigsool.com>
2021-04-19 16:06:39 +0200

    Prepare admin UI

--- src/parts/x-admin.ts
+++ src/parts/x-admin.ts
@@ -179,58 +179,59 @@ export class XAdmin extends Localized(LitElement) {
 
   private _adminNav(): TemplateResult {
     return html`
-    <nav class="bg-gray-600">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="grid grid-flow-col grid-col-2 p-2">
-          <div class="ml-10 flex space-x-4 items-center">
-            <a href="${XAdmin.MainPathPrefix}/${DashboardPages.home}" class="px-3 py-2 rounded-md text-sm font-medium ${this._page === DashboardPages.home ? 'bg-gray-900 text-white' : 'text-gray-300'}">${msg('Dashboard')}</a>
-            <a href="${XAdmin.MainPathPrefix}/${DashboardPages.pages}" class="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${this._page === DashboardPages.pages ? 'bg-gray-900 text-white' : 'text-gray-300'}">${msg('Pages')}</a>
-            <a href="${XAdmin.MainPathPrefix}/${DashboardPages.settings}" class="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${this._page === DashboardPages.settings ? 'bg-gray-900 text-white' : 'text-gray-300'}">${msg('Settings')}</a>
-            <a href="${XAdmin.MainPathPrefix}/${DashboardPages.reports}" class="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${this._page === DashboardPages.reports ? 'bg-gray-900 text-white' : 'text-gray-300'}">${msg('Reports')}</a>
-            <a href="${XAdmin.MainPathPrefix}/${DashboardPages.assets}" class="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${this._page === DashboardPages.assets ? 'bg-gray-900 text-white' : 'text-gray-300'}">${msg('Assets')}</a>
-
-          </div>
-          <div class="grid grid-flow-col grid-col-2 gap-4 justify-center">
+    <div class="flex">
+        <div class="flex flex-col items-center w-16 h-screen overflow-hidden text-indigo-300 terra-bg">
+          <div class="flex flex-col items-center mt-3">
             ${this._person ? html`
-            <img class="h-10 w-10 bg-white rounded-lg" src="https://avatars.dicebear.com/api/bottts/${this._person?.profile().stxAddress.mainnet}.svg" />
+              <img class="h-10 w-10 bg-white rounded-lg" src="https://avatars.dicebear.com/api/bottts/${this._person?.profile().stxAddress.mainnet}.svg" />
             ` : html``}
-            <a title="${msg('Logout')}" @click=${() => {
-                this._userSession?.signUserOut();
-                this._signedIn = false;
-              }} class="group cursor-pointer relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white terra-bg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                <span class="flex items-center">
-                  <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
-                  </svg>
-                </span>
+            <a class="flex items-center justify-center w-12 h-12 mt-2 rounded ${this._page === DashboardPages.home ? 'text-indigo-100 bg-blue-700' : 'hover:bg-blue-700 hover:text-white'}" href="${XAdmin.MainPathPrefix}/${DashboardPages.home}" title="${msg('Dashboard')}">
+              <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+              </svg>
+            </a>
+            <a class="flex items-center justify-center w-12 h-12 mt-2 rounded ${this._page === DashboardPages.pages ? 'text-indigo-100 bg-blue-700' : 'hover:bg-blue-700 hover:text-white'}" href="${XAdmin.MainPathPrefix}/${DashboardPages.pages}" title="${msg('Dashboard')}">
+              <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
+              </svg>
+            </a>
+            <a class="flex items-center justify-center w-12 h-12 mt-2 rounded ${this._page === DashboardPages.reports ? 'text-indigo-100 bg-blue-700' : 'hover:bg-blue-700 hover:text-white'}" href="${XAdmin.MainPathPrefix}/${DashboardPages.reports}" title="${msg('Reports')}">
+              <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              </svg>
             </a>
           </div>
+          <a class="flex items-center justify-center w-12 h-12 mt-2 rounded ${this._page === DashboardPages.settings ? 'text-indigo-100 bg-blue-700' : 'hover:bg-blue-700 hover:text-white'}" href="${XAdmin.MainPathPrefix}/${DashboardPages.settings}" title="${msg('Settings')}">
+            <svg class="w-6 h-6 stroke-current"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+            </svg>
+          </a>
+          <a title="${msg('Logout')}" @click=${() => {
+              this._userSession?.signUserOut();
+              this._signedIn = false;
+            }} class="flex items-center justify-center w-12 h-12 mt-2 rounded cursor-pointer hover:bg-blue-700 hover:text-white">
+              <span class="flex items-center">
+                <svg class="h-5 w-5 hover:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                </svg>
+              </span>
+          </a>
+        </div>
+        <div class="container px-4 py-6 col-span-1">
+          ${this._pageForTitle(this._page)}
         </div>
       </div>
-    </nav>
+    </div>
     `;
   }
 
   render(): TemplateResult {
-    return html`
-
+    return html`	
     ${this._signedIn ? html`
-    ${this._adminNav()}
-    ` : html``}
-
-    <main>
-      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <div class="px-4 py-6 sm:px-0">
-          <div class="rounded-lg h-96">
-            ${this._pageForTitle(this._page)}
-
-            ${!this._signedIn ? html`
-            ${this._connectButton()}
-            ` : html``}
-          </div>
-        </div>
-      </div>
-    </main>
+      ${this._adminNav()}
+    ` : html`
+      ${this._connectButton()}
+    `}
     `;
   }
 
