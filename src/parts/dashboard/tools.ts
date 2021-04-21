cb847756df8156b6d1efe5cd0b933a16a78c19b3
Léonard <leonard@bigsool.com>
2021-04-22 01:19:42 +0200

    fixes

--- src/parts/dashboard/tools.ts
+++ src/parts/dashboard/tools.ts
@@ -35,6 +35,15 @@ export class WebsiteTools extends Localized(LitElement) {
     this.loading = false;
   }
 
+  private _openBox(e: Event) {
+    const editable = (e.currentTarget as HTMLElement).nextElementSibling;
+    if (editable?.classList.contains('hidden')) {
+      editable?.classList.remove('hidden');
+    } else {
+      editable?.classList.add('hidden');
+    }
+  }
+
   render(): TemplateResult {
     return html`
         <div class="flex justify-between gap-2 ml-4 mb-4 pb-6">
@@ -58,32 +67,43 @@ export class WebsiteTools extends Localized(LitElement) {
             </h2>
             ${this._tools && this._tools.map(tool => {
               return html`
-              <div class="border-4 rounded-md p-2 m-2">
-                <label class="leading-7 text-sm text-gray-600">${tool.id}</label>
-                <div class="relative">
-                    <input name="${tool.id}-name" id="${tool.id}-name" .value=${tool.name} type="text" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    <textarea placeholder="Explain" name="${tool.id}-name" id="${tool.id}-explain" .value=${tool.explain} class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
+              <div class="w-full bg-gray-100 cursor-pointer mb-2 select-none" @click=${this._openBox}>
+                <div class="rounded flex p-4 h-full items-center justify-between">
+                  <span class="title-font font-medium">${tool.name}</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                  </svg>
+                </div>
+              </div>
+              <div class="p-2 m-2 rounded hidden">
+                <input name="${tool.id}-name" id="${tool.id}-name" .value=${tool.name} type="text" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                <textarea placeholder="Explain" name="${tool.id}-name" id="${tool.id}-explain" .value=${tool.explain} class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
 
-                    <div class="bg-gray-100 p-4">
-                      ${tool.links.map((link, idx) => {
-                        return html`
-                        <div class="flex">
-                          <div class="relative">
-                            <label for="${tool.id}-link-${idx}-title" class="leading-7 text-sm text-gray-600">Title</label>
-                            <input name="${tool.id}-link-${idx}-title" id="${tool.id}-link-${idx}-title" type="text" .value=${link.name} class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                          </div>
-                          <div class="relative">
-                            <label for="${tool.id}-link-${idx}-href" class="leading-7 text-sm text-gray-600">URL</label>
-                            <input name="${tool.id}-link-${idx}-href" id="${tool.id}-link-${idx}-href" type="text" .value=${link.href} class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                          </div>
-                        </div>
-                        `;
-                      })}
+                <div class="tool-links flex flex-wrap">
+                  ${tool.links.map((link, idx) => {
+                    return html`
+                    <div class="flex">
+                      <div class="relative">
+                        <label for="${tool.id}-link-${idx}-title" class="leading-7 text-sm text-gray-600">Title</label>
+                        <input name="${tool.id}-link-${idx}-title" id="${tool.id}-link-${idx}-title" type="text" .value=${link.name} class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                      </div>
+                      <div class="relative">
+                        <label for="${tool.id}-link-${idx}-href" class="leading-7 text-sm text-gray-600">URL</label>
+                        <input name="${tool.id}-link-${idx}-href" id="${tool.id}-link-${idx}-href" type="text" .value=${link.href} class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                      </div>
                     </div>
-                  </div>
+                    `;
+                  })}
+                  <button type="button" class="w-full justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-white terra-bg">
+                    ${msg('Add link')}
+                  </button>
+                </div>
               </div>
               `;
-            })}    
+            })}
+            <button type="button" class="w-full justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-white terra-bg">
+              ${msg('Add tool')}
+            </button>
           `}
         </div>
     `;
