e463c8b25138f0f6898b367ea1d2ec6c998ebba9
Léonard <leonard@bigsool.com>
2021-04-21 13:22:22 +0200

    Cleanup page loader

--- src/parts/dashboard/pages.ts
+++ src/parts/dashboard/pages.ts
@@ -30,16 +30,8 @@ export class WebsitePages extends Localized(LitElement) {
     return this;
   }
 
-  async loadFiles(): Promise<void> {
-    this._files = systemPages;
-  }
-
   async firstUpdated(): Promise<void> {
-    await this.loadFiles();
-  }
-
-  public get editedPage(): string {
-    return `page-${this.page}-${this.lang}.json`;
+    this._files = systemPages;
   }
 
   private async _savePage() {
@@ -103,7 +95,6 @@ export class WebsitePages extends Localized(LitElement) {
             </button>
             <button @click=${async () => {
               console.warn('not deleted.');
-              await this.loadFiles();
             }} class="bg-blue-500 hover:terra-bg text-white py-2 px-4 rounded">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
