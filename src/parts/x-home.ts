210b10001c395d87f9123d8d4e87ab480c569d55
Léonard <leonard@bigsool.com>
2021-04-22 19:04:07 +0200

    add shadow

--- src/parts/x-home.ts
+++ src/parts/x-home.ts
@@ -94,7 +94,7 @@ export class XHome extends Localized(LitElement) {
     return html`
       <div class="xl:w-1/3 md:w-1/2 p-4">
         <div
-          class="border border-gray-300 p-6 rounded-lg strength hover:text-white"
+          class="border border-gray-300 p-6 rounded-lg strength hover:text-white hover:shadow-lg"
         >
           <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
             ${title}
