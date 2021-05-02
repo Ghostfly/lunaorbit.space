67f1258be6a0355c572d700f3aa2eb2f402a5ba6
Léonard <leonard@bigsool.com>
2021-05-02 15:03:48 +0200

    remove bad nav move

--- src/components/airdrop-dialog.ts
+++ src/components/airdrop-dialog.ts
@@ -8,7 +8,6 @@ import {
 } from 'lit-element';
 import {msg} from '@lit/localize';
 import {AnchorClaimResponse, MIRAirdrop, TerraQuery} from '../terra/terra-min';
-import {Router} from '@vaadin/router';
 
 /**
  * Airdrop dialog component
@@ -262,7 +261,6 @@ export class AirdropDialog extends LitElement {
               }
               <button @click=${() => {
                 this.close();
-                Router.go('home');
               }} type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                 ${msg('Close')}
               </button>
